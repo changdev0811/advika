0db90dbb4a413d3723214c02e7ed3273141b5ac4
smith0811 <anywhere0811@outlook.com>
2019-04-10 14:23:22 +0800

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     // mongoURI: 'mongodb://localhost:27017/holodeck-axiado',
-    mongoURI: 'mongodb://smith0811:Testtest_01@cluster0-shard-00-00-dvaqf.mongodb.net:27017,cluster0-shard-00-01-dvaqf.mongodb.net:27017,cluster0-shard-00-02-dvaqf.mongodb.net:27017/holodeck-axiado?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=tru',
+    mongoURI: 'mongodb://smith0811:Testtest_01@cluster0-shard-00-00-dvaqf.mongodb.net:27017,cluster0-shard-00-01-dvaqf.mongodb.net:27017,cluster0-shard-00-02-dvaqf.mongodb.net:27017/holodeck-axiado?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     secretOrKey: 'intrusion'
 }
